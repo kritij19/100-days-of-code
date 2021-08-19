414df0548e133277bce8936c9f0fe8f05f7f2d97
Kriti Jain <76650361+kritij19@users.noreply.github.com>
2021-08-19 14:51:28 +0530

    Add files via upload

---
+++ Quiz_using_OOP/question_model.py
@@ -0,0 +1,10 @@
+# Format for holding questions and answers.
+
+class Question:
+     
+     def __init__(self, question, answer):
+         self.question = question
+         self.answer = answer
+
+
+
---
+++ Quiz_using_OOP/quiz_brain.py
@@ -0,0 +1,30 @@
+class QuizBrain:
+
+    def __init__(self, question_list):
+        self.question_list = question_list #List of questions
+        self.question_number = 0
+        self.score = 0
+    
+    # Prints the question corresponding to that question number
+    # By adding check_answer also evaluates the guess.
+    def next_question(self):
+        question = self.question_list[self.question_number] 
+        question_text = question.question
+        self.question_number += 1
+        user_answer = input(f"Q{self.question_number}. {question_text} (True/False): ") 
+        self.check_answer(user_answer, question.answer)
+    
+    # Checks if the quiz still has question remaining
+    # Returns False if there are no questions left
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+    
+
+    # If the answer entered by the user matches the actual answer, score is increased by 1.  
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You're answer is correct")
+            self.score += 1
+        else:
+            print("You have got it wrong")
+        print(f"Your current score: {self.score}/ {self.question_number}\n")
