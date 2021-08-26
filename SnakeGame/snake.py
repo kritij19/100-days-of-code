81c321678dc7dfe1ba71f92594283bf29cf9cbb3
Kriti Jain <76650361+kritij19@users.noreply.github.com>
2021-08-26 09:47:36 +0530

    Update snake.py

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -7,29 +7,34 @@ DOWN = 270
 RIGHT = 0
 LEFT = 180
 
-'''Responsible for the following functionionalities:
+'''Capable of performing the following functions:
     1. Creating the snake
     2. Moving the snake (The snake moves forward unless interuppted)
     3. Changing the direction of the snake (The snake cannot move backward)
-    4. Increasing the length of the snake as score increases.'''
+    4. Increasing the length of the snake as score increases.
+    5. Reseting the snake to original position in original length.'''
     
 class Snake:
 
     def __init__(self):
+
         # Creating the snake
         turtle_list = []
         self.turtle_list = turtle_list
         
-        for i in range(INITIAL_SNAKE_LENGTH):
-            self.add_segment((-20 * i, 0))
-        
+        self.create_snake()
         self.head = turtle_list[0]
         # Update once the snake has been created 
         Screen().update()
     
+    def create_snake(self):
+
+        for i in range(INITIAL_SNAKE_LENGTH):
+            self.add_segment((-20 * i, 0))
+
     # Adds a segment to the specified position. Appends a turtle object to the turtle_list.
     def add_segment(self, position):
-        
+
             self.turtle_list.append(Turtle(shape ='square'))
             self.turtle_list[-1].penup()
             self.turtle_list[-1].color('white')
@@ -37,7 +42,7 @@ class Snake:
     
     # Increases length when point scored.Position where added = position of previous tail.
     def increase_length(self):
-        
+
             self.add_segment(self.turtle_list[-2].position())
 
     # Moves the last turtle to the second last turtle's position and so on.
@@ -45,14 +50,27 @@ class Snake:
     def move(self):
 
         turtle_list = self.turtle_list 
+
         for i in range(len(turtle_list) - 1, 0, -1):
             x = turtle_list[i - 1].xcor()
             y = turtle_list[i - 1].ycor()
             turtle_list[i].goto(x, y)
+
         self.head.forward(MOVE_DISTANCE)
+
+    # Reseting the snake to original position in original size.
+    def reset(self):
+        # Current segements off the screen
+        for segment in self.turtle_list:
+            segment.goto(1000, 1000)
+        
+        self.turtle_list.clear()
+        self.create_snake()
+        self.head = self.turtle_list[0]
 
     # Turning the turtle
     def up(self):
+
         # The turtle can not move backwards
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
