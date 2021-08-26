59b73c928b5bdf30cbde29d261b6ec0825dd91f1
Kriti Jain <76650361+kritij19@users.noreply.github.com>
2021-08-26 09:44:47 +0530

    Update scoreboard.py

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -6,9 +6,8 @@ ALIGNMENT = 'center'
 '''Responsible for the following functionalities:
     1. Displaying the score.
     2. Increasing the score.
-    3. Displaying Game over at the centre of the screen'''
+    3. Reseting score to 0 in each game and controlling high score.'''
 
-# Score is a turtle object.
 class Score(Turtle):
 
     def __init__(self):
@@ -17,13 +16,30 @@ class Score(Turtle):
         self.color("white")
         self.hideturtle()
         self.penup()
+
+        # To retain the previous high score
+        with open('data.txt',mode = 'r') as file:
+            self.highscore = int(file.read())
+
+    # After each game over. Reset score to 0 and change high score if current score greater than prev high score.
+    def reset(self):
+        if self.score > self.highscore:
+                self.highscore = self.score
+                # Storing high score in word file so that it's retained.
+                # Overwriting the new high score into the file.
+                with open('data.txt',mode = 'w') as file:
+                    file.write(str(self.highscore))
+                
+            
+        self.display_score()
+        self.score = 0
         
     def display_score(self):
+        self.clear()
         self.goto(-20, 270)
-        self.write(f"SCORE: {self.score}", align = ALIGNMENT, font = FONT)
+        self.write(f"SCORE: {self.score} HIGH SCORE: {self.highscore}", align = ALIGNMENT, font = FONT)
 
     def increase_score(self):
-        self.clear()
         self.score += 1
         self.display_score()
 
