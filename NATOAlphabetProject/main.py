1444e5cf5f03d263364d276211875631b0abd3d9
Kriti Jain <76650361+kritij19@users.noreply.github.com>
2021-08-29 23:40:37 +0530

    Update main.py

--- NATOAlphabetProject/main.py
+++ NATOAlphabetProject/main.py
@@ -1,17 +1,25 @@
 
-import pandas as pd
-
-#TODO 1. Create a dictionary of {letter: word} from csv:
-
-data = pd.read_csv(r'NATO-alphabet-start\nato_phonetic_alphabet.csv')
+data = pd.read_csv(r'nato_phonetic_alphabet.csv')
 
 # Dict with a letters as key and words as value
+
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-# Adding words from phonetic_dict whose key is same as letters in the name
-output_list = [phonetic_dict[letter] for letter in word]
+program_is_on = True
 
-print(output_list)
+# Keep asking for input unless a valid input is entered.
+while program_is_on:
+
+    word =  input("Enter a word: ").upper()
+    
+    # Adding words from phonetic_dict whose key is same as letters in the name
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters in the alphabets please!")
+    else:
+        print(output_list)
+        program_is_on = False
+
